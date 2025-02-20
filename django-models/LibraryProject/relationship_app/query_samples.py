484841075c64106b06b0d5da2cbb5a4294396af7
Developer-Linus <linusmksu30@gmail.com>
2025-02-20 09:11:25 +0300

    completed task 0

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,5 +4,5 @@ books = author.books.all()
 book = Book.objects.get(title='12 Rules for Life')
 books = book.libraries.all()
 
-library = Library.objects.get(name="Machakos University")
+library = Library.objects.get(name='Machakos University')
 librarian = library.librarian.get(name="Jack")
