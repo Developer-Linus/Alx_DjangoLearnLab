a4ef3bc916810f8331fe9249bfa456a3c311f874
Developer-Linus <linusmksu30@gmail.com>
2025-02-20 09:23:11 +0300

    completed task 0

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,8 @@
-author = Author.objects.get(name='author_name')
+author = Author.objects.get(name=author_name)
 books = author.books.all()
 
-library = Library.objects.get(name='library_name')
+library = Library.objects.get(name=library_name)
 books = library.libraries.all()
 
-library = Library.objects.get(name='library_name')
-librarian = library.librarian.get(name="librarian_name")
+library = Library.objects.get(name=library_name)
+librarian = library.librarian.get(name=librarian_name)
