9ab13211bb577dfa5e03081fc9b24ca96bd9679a
Developer-Linus <linusmksu30@gmail.com>
2025-02-21 09:44:00 +0300

    completed task 2

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,13 +2,14 @@ from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books
 from .views import LibraryDetailView
-from .views import RegisterView
+from .views import login_view, logout_view, register_view, home_view
 
 urlpatterns = [
     path('books/details/', list_books, name='book-details'),
     path('library/books/', LibraryDetailView.as_view(), name='library-books'),
+    path('', home_view, name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path("register/", RegisterView.as_view(), name="register"),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
@@ -7,8 +8,12 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login
 from django.contrib.auth import logout
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
 
-
+def home_view(request):
+    return HttpResponse('Welcome to library app.')
 def list_books(request):
     books = Book.objects.all()
     context = {'book_list': books}
@@ -17,9 +22,35 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
 
-class RegisterView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/register.html"
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirect to a home
+        else:
+            messages.error(request, 'Invalid username or password.')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+def logout_view(request):
+    logout(request)
+    return redirect('registration/login')  # Redirect to the login page after logout
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after registration
+            return redirect('home')  # Redirect to a success page (e.g., home)
+        else:
+            messages.error(request, 'Error during registration. Please check your input.')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 
