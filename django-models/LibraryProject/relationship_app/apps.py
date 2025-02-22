0bae5f767189baf61f82da2dfa58422e847075e2
Developer-Linus <linusmksu30@gmail.com>
2025-02-22 14:11:42 +0300

    completed task 3

--- django-models/LibraryProject/relationship_app/apps.py
+++ django-models/LibraryProject/relationship_app/apps.py
@@ -6,4 +6,4 @@ class RelationshipAppConfig(AppConfig):
     name = 'relationship_app'
 
     def ready(self):
-        import relationship_app.signals  # Import signals when the app starts
+        import signals  # Import signals when the app starts
--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -33,5 +35,14 @@ class UserProfile(models.Model):
     roles = models.CharField(max_length=20, choices = ROLE_CHOICES, default='Member')
     def __str__(self):
         return f'{self.user.username} - {self.role}'
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
 
 
