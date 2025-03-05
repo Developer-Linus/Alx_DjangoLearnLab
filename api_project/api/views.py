f7edd906dea623b00610045dd24a87b0e4df295b
Developer-Linus <linusmksu30@gmail.com>
2025-03-05 09:33:27 +0300

    completed task 3 where I implemented authentication and permissions for my views

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,12 +2,15 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 class BookList(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
+
